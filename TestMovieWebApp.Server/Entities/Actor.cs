f58b333f76042c422e55373c406c74c4926800a6
Jan Hodic <hodic@ditec.sk>
2025-01-29 15:16:37 +0100

    model done

--- TestMovieWebApp.Server/Entities/Actor.cs
+++ TestMovieWebApp.Server/Entities/Actor.cs
@@ -5,5 +5,7 @@ namespace TestMovieWebApp.Server.Entities
     public class Actor: BaseIdentity
     {
         public string Name { get; set; } = string.Empty;
+        public virtual Guid? MovieId { get; set; }
+        public virtual Movie? Movie { get; set; }
     }
 }
