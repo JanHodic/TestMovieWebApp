be0c5d3964591c654b8c6436af5f646504e765ef
Jan Hodic <hodic@ditec.sk>
2025-01-30 15:07:10 +0100

    mappings added

--- TestMovieWebApp.Server/Services/Commons/AutomapperConfigurationProfile.cs
+++ TestMovieWebApp.Server/Services/Commons/AutomapperConfigurationProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using TestMovieWebApp.Server.Dtos;
 using TestMovieWebApp.Server.Entities;
+using TestMovieWebApp.Server.Rtos;
 
 namespace TestMovieWebApp.Server.Services.Commons
 {
@@ -12,6 +13,14 @@ namespace TestMovieWebApp.Server.Services.Commons
                 .ForMember(d => d.Actors, opt => opt.MapFrom(m => m.ActorIds));
             CreateMap<Movie, MovieDto>()
                 .ForMember(d => d.ActorIds, opt => opt.MapFrom(m => m.Actors));
+            CreateMap<ActorDto, Actor>()
+                .ForMember(d => d.Movie, opt => opt.MapFrom(m => m.MovieId));
+            CreateMap<Actor, ActorDto>()
+                .ForMember(d => d.MovieId, opt => opt.MapFrom(m => m.Movie));
+            CreateMap<ActorRto, Actor>()
+                .ForMember(d => d.Movie, opt => opt.MapFrom(m => m.Movie));
+            CreateMap<MovieRto, Movie>()
+                .ForMember(d => d.Actors, opt => opt.MapFrom(m => m.Actors));
         }
     }
 }
