62bc4d2776eaa9b8240505e2537ddec33e89f453
Jan Hodic <hodic@ditec.sk>
2025-01-31 15:21:20 +0100

    dto mappings missing

--- TestMovieWebApp.Server/Presentations/Controllers/FileController.cs
+++ TestMovieWebApp.Server/Presentations/Controllers/FileController.cs
@@ -21,7 +21,7 @@ namespace TestMovieWebApp.Server.Presentations.Controllers
         }
 
         [HttpGet]
-        public async Task<ICollection<Movie>> ReadFile()
+        public async Task<ICollection<MovieDto>> ReadFile()
         {
             var contents = await _service.ReadFile();
             return contents;
